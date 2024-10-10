41793b5561cacef349549895d3d566f612ec5416
Aaditya <aadityathapa977@gmail.com>
2024-10-10 22:10:58 +0545

    Modified the UI

--- Dropdown/script.js
+++ Dropdown/script.js
@@ -1,33 +1,38 @@
-let dropDownContainer = document.querySelector(".dropdown-container");
-let selectionItems = document.querySelector(".dropdown-sub-items");
-let dropDownItems = document.querySelectorAll(".dropdown-items");
-let isVisible
-console.log(dropDownItems);
-let mainItem = document.querySelector(".dropdown-main-item");
-dropDownContainer.addEventListener("click", () => {
-   isVisible = !isVisible; // Toggle the visibility state
-   selectionItems.style.display = isVisible ? "flex" : "none"; // Set display based on visibility state
-});
+// let dropDownContainer = document.querySelector(".dropdown-container");
+// let selectionItems = document.querySelector(".dropdown-sub-items");
+// let dropDownItems = document.querySelectorAll(".dropdown-items");
+// let isVisible
+// console.log(dropDownItems);
+// let mainItem = document.querySelector(".dropdown-main-item");
+// dropDownContainer.addEventListener("click", () => {
+//    isVisible = !isVisible; // Toggle the visibility state
+//    selectionItems.style.display = isVisible ? "flex" : "none"; // Set display based on visibility state
+// });
 
-function changeMainItem(item) {
-   console.log(item);
-   item.addEventListener("click", () => {
-      let currentMainItem = mainItem.innerHTML;
-      mainItem.innerHTML = item.innerHTML;
-      item.remove();
-      const newItem = document.createElement("div");
-      newItem.classList.add("dropdown-items");
-      newItem.innerHTML = currentMainItem;
-      selectionItems.append(newItem);
-      changeMainItem(newItem);
-   });
-}
-dropDownItems.forEach((item) => {
-   changeMainItem(item);
-});
-document.addEventListener("click", (event) => {
-   if (!dropDownContainer.contains(event.target)) {
-      selectionItems.style.display = "none";
-      isVisible = false; // Update visibility state
-   }
+// function changeMainItem(item) {
+//    console.log(item);
+//    item.addEventListener("click", () => {
+//       let currentMainItem = mainItem.innerHTML;
+//       mainItem.innerHTML = item.innerHTML;
+//       item.remove();
+//       const newItem = document.createElement("div");
+//       newItem.classList.add("dropdown-items");
+//       newItem.innerHTML = currentMainItem;
+//       selectionItems.append(newItem);
+//       changeMainItem(newItem);
+//    });
+// }
+// dropDownItems.forEach((item) => {
+//    changeMainItem(item);
+// });
+// document.addEventListener("click", (event) => {
+//    if (!dropDownContainer.contains(event.target)) {
+//       selectionItems.style.display = "none";
+//       isVisible = false; // Update visibility state
+//    }
+// });
+let dropDownBtn = document.querySelector(".dropdown-select-item");
+dropDownBtn.addEventListener("click", () => {
+   dropDownBtn.classList.toggle("active");
+   document.querySelector(".dropdown-options").classList.toggle("visible");
 });
