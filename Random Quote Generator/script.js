7faabcde64493795fc3182759e23505a9f7effdd
Aaditya <aadityathapa977@gmail.com>
2024-10-11 20:02:18 +0545

    Added error popup, added new select interface

--- Random Quote Generator/script.js
+++ Random Quote Generator/script.js
@@ -5,8 +5,33 @@ let author = document.querySelector(".quote-author");
 let generateQuoteBtn = document.querySelector(".new-quote-btn");
 let copyBtn = document.querySelector(".copy-quote");
 let currentCategory = document.querySelector("#current-category");
+let dropDownContainer = document.querySelector(".dropdown-select-item");
+let mainItem = document.querySelector(".select");
+let options = document.querySelectorAll("#option li");
+let popUp = document.querySelector(".pop-up");
+let closePopUpBtn = document.querySelector("#close-btn");
+dropDownContainer.addEventListener("click", () => {
+   dropDownContainer.classList.toggle("active");
+   document.querySelector(".dropdown-options").classList.toggle("visible");
+});
+options.forEach((option) => {
+   option.addEventListener("click", () => {
+      mainItem.innerHTML = `${option.innerHTML}`;
+   });
+});
+document.addEventListener("click", (e) => {
+   if (!dropDownContainer.contains(e.target)) {
+      dropDownContainer.classList.remove("active"); // Remove active class if clicking outside
+      document.querySelector(".dropdown-options").classList.remove("visible"); // Hide dropdown
+   }
+});
+
 async function fetchQuotes() {
-   let currentCategoryValue = currentCategory.value;
+   let currentCategoryValue = mainItem.innerHTML.trim();
+   if (currentCategoryValue == "Select Category") {
+      showPopUp();
+   }
+   // console.log(currentCategoryValue);
    try {
       const response = await fetch(
          `https://api.api-ninjas.com/v1/quotes?category=${currentCategoryValue}`,
@@ -41,4 +66,18 @@ copyBtn.addEventListener("click", async () => {
    } catch (err) {
       console.log(err.message);
    }
+});
+function showPopUp() {
+   setTimeout(() => {
+      popUp.style.display = "flex";
+      popUp.classList.add("scale-in");
+      popUp.classList.remove("scale-out");
+   }, 800);
+}
+closePopUpBtn.addEventListener("click", () => {
+   popUp.classList.remove("scale-in");
+   popUp.classList.add("scale-out");
+   setTimeout(() => {
+      popUp.style.display = "none";
+   }, 300); //Added 300ms delay for smoother animation
 });
